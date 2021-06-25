eac0885e25ef54692845054a1c12b9553c3d94f7
Muhammad Taufiq <taufiq@siklus.id>
2021-06-25 17:05:10 +0700

    first commit

---
+++ lib/custom_colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color darkBlue = Color(0XFF2c2c54);
+Color darkSilver = Color(0XFF84817a);
---
+++ lib/font_style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/custom_colors.dart';
+
+TextStyle mainHeader = TextStyle(
+    fontSize: 26,
+    color: darkBlue,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700);
+TextStyle subHeader = TextStyle(
+    fontSize: 16,
+    color: darkSilver,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w300);
