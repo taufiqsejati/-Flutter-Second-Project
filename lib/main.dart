af66f42aa5642f9a5e761778b1d35f0da703e22c
Taufiqsejati <taufiq@sikluid>
2021-06-30 23:08:57 +0700

    API POST

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/login_page.dart';
+import 'package:flutter_application_1/post_result_model.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  PostResult postResult = null;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Api Demo'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text((postResult != null)
+                  ? postResult.id +
+                      " | " +
+                      postResult.name +
+                      " | " +
+                      postResult.job +
+                      " | " +
+                      postResult.created
+                  : 'Tidak ada data'),
+              RaisedButton(
+                onPressed: () {
+                  PostResult.connectToApi('Badu', 'dokter').then((value) {
+                    postResult = value;
+                    setState(() {});
+                  });
+                },
+                child: Text('POST'),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/main_part_2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
