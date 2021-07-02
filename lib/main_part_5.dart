40935bb5f6654cce2756ceae31a3c39ddb77613d
Muhammad Taufiq <taufiq@siklus.id>
2021-07-02 17:09:38 +0700

    update bloc

--- lib/main_part_5.dart
+++ lib/main_part_5.dart
@@ -9,12 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: BlocProvider<ColorBloc>(
-        builder: (context) => ColorBloc(),
-        child: MainPage(),
-      ),
-    );
+    return MaterialApp();
   }
 }
 
